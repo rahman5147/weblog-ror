fc829d4f094bcfe54001297fb7757bb01ca0c98a
Rahman <rahmanromli@yahoo.com>
2017-09-04 21:03:23 +0800

    Using partial for new & edit post form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post#index', as: 'home'
   get "about" => "page#about"
-  resources :post
+  resources :post, as: 'posts'	# Fix plural _path
 end
