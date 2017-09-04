d917cfccbad06dd0abc4735ba942db444910bc3b
Rahman <rahmanromli@yahoo.com>
2017-09-04 08:50:37 +0800

    Save post & show post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,10 +2,19 @@ class PostController < ApplicationController
 	def index
 	end
 
+	def show
+	end
+
 	def new
 	end
 
 	def create
-		@post = Post.new(params[:post])
+		@post = Post.new(post_params)
+		@post.save
+		redirect_to @post
+	end
+
+	private def post_params
+		params.require(:post).permit(:title, :body)
 	end
 end
